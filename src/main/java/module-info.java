197962bbc4fe561a26c0daf8d5d49d83eb878338
Simon-James <201732050@collegealma.ca>
2024-02-07 14:03:10 -0500

    Premier commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.calma.ui.s_4204p2aa_201732050 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.calma.ui.s_4204p2aa_201732050 to javafx.fxml;
+    exports org.calma.ui.s_4204p2aa_201732050;
+}
---
+++ src/main/java/org/calma/ui/s_4204p2aa_201732050/HelloController.java
@@ -0,0 +1,14 @@
+package org.calma.ui.s_4204p2aa_201732050;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
