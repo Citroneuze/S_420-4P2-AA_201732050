64ae6ba6be3efe17121d71328ad8071a6a6aecb3
Simon-James <201732050@collegealma.ca>
2024-02-12 12:14:33 -0500

    deuxieme commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.calma.ui.s_4204p2aa_201732050 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.calma.ui.s_4204p2aa_201732050 to javafx.fxml;
-    exports org.calma.ui.s_4204p2aa_201732050;
-}
---
+++ src/main/java/org/calma/ui/s_4204p2aa_201732050/Partie2/InterfaceXML.java
@@ -0,0 +1,4 @@
+package org.calma.ui.s_4204p2aa_201732050.Partie2;
+
+public class InterfaceXML {
+}
